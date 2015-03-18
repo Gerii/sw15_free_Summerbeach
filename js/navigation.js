d4beaddcc53feb0dc044eff477211e55f09429fa
Angela Promitzer <angelapromitzer@tug-swl-d98.tu-graz.ac.at>
2015-03-18 10:32:48 +0100

    [PA, PF, PR] {test after code} implementation of browser back

--- js/navigation.js
+++ js/navigation.js
@@ -9,9 +9,10 @@ $(document).on("pagebeforeshow", function() {
 });
 
 Navigation = function() {
-	this.loadPage("home.html");
+//	this.loadPage("home.html");
 	if (arguments.callee._singletonInstance)
 		return arguments.callee._singletonInstance;
+	this.loadPage("home.html");
 	arguments.callee._singletonInstance = this;
 };
 
@@ -41,9 +42,16 @@ Navigation.prototype.loadPage = function(url) {
 	}, "html");
 };
 
-/*$(window).on("navigate", function(event, data) {
+Navigation.prototype.buildUrl = function(hash) {
+    return hash.substr(1,hash.length) + ".html" ;
+};
+
+$(window).on("navigate", function(event, data) {
 	if (data.state.direction === "back") {
-		$.post("test2.html", {
+		var nav = new Navigation();
+
+		var hash = window.location.hash;
+		$.post(nav.buildUrl(hash), {
 			sort : "",
 			page : ""
 		}, function(result) {
@@ -56,4 +64,4 @@ Navigation.prototype.loadPage = function(url) {
 		console.log(data.state.url);
 		console.log(data.state.hash);
 	}
-});*/
+});
