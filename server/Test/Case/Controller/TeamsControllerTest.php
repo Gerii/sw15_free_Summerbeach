3638de97a4f40176db49b3c163a821cfcf3f6e3d
Gerald Palfinger <palfinger@student.tugraz.at>
2015-04-01 10:20:03 +0200

    [PA,PF,PR]{test before code} Server test for adding teams

--- server/Test/Case/Controller/TeamsControllerTest.php
+++ server/Test/Case/Controller/TeamsControllerTest.php
@@ -7,22 +7,35 @@ App::uses('TeamsController', 'Controller');
  */
 class TeamsControllerTest extends ControllerTestCase {
 
-/**
- * Fixtures
- *
- * @var array
- */
-	public $fixtures = array(
-		'app.team'
-	);
+	/**
+	 * Fixtures
+	 *
+	 * @var array
+	 */
+	public $fixtures = array('app.team');
 
-/**
- * testAddteam method
- *
- * @return void
- */
+	public function setUp() {
+		parent::setUp();
+
+		// Load Contact Model
+		$this -> Team = ClassRegistry::init('Team');
+	}
+
+	/**
+	 * testAddteam method
+	 *
+	 * @return void
+	 */
 	public function testAddteam() {
-            
+		$team = "blub";
+		$school = "bluuub";
+		$data = array('name' => $team, 'school' => $school, 'members' => array());
+		$before = $this -> Team -> find('count', array('conditions' => array('Team.teamname' => $team, 'Team.schule' => $school)));
+		$this -> testAction('/Teams/addteam.json', array('data' => $data, 'method' => 'post'));
+		$after = $this -> Team -> find('count', array('conditions' => array('Team.teamname' => $team, 'Team.schule' => $school)));
+
+		// Test DB entry
+		$this -> assertEqual($after - $before, 1);
 	}
 
 }
