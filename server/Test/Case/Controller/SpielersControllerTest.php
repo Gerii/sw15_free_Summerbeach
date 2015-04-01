75b0e04165fffe7db7e358afa3e776afd530668f
Gerald Palfinger <palfinger@student.tugraz.at>
2015-04-01 12:38:02 +0200

    [PA,PF,PR]{test before code} Add test for wrong parameters

--- server/Test/Case/Controller/SpielersControllerTest.php
+++ server/Test/Case/Controller/SpielersControllerTest.php
@@ -24,9 +24,9 @@ class SpielersControllerTest extends ControllerTestCase {
 
 	public function testAddMember() {
 		$team_id = 1;
-		$spielernummer = 1;
+		$playernumber = 1;
 		$firstname = "Johann";
-		$lastname = "Mustermann";
+		$secondname = "Mustermann";
 		$dateofbirth = "05.10.1995";
 		$phone = "2342564";
 		$address = "Inffeldgasse";
@@ -35,18 +35,53 @@ class SpielersControllerTest extends ControllerTestCase {
 		$email = "johann.mustermann@email.at";
 		$gender = "m";
 		$tshirt = "s";
+		$data = array('firstname' => $firstname, 'secondname' => $secondname, 'dateofbirth' => $dateofbirth, 'phone' => $phone, 'address' => $address, 'zip' => $zip, 'location' => $location, 'email' => $email, 'gender' => $gender, 'tshirt' => $tshirt);
+
+		$this -> assertEqual($this -> insertMember($team_id, $data, $playernumber), 1);
+	}
+
+	private function insertMember($team_id, $data, $playernumber) {
+		$query = array('Spieler.team_id' => $team_id, 'Spieler.spielernummer' => $playernumber, 'Spieler.vorname' => $data["firstname"], 'Spieler.nachname' => $data["secondname"], 'Spieler.geburtsdatum' => $data["dateofbirth"], 'Spieler.telefon' => $data["phone"], 'Spieler.strasse' => $data["address"], 'Spieler.plz' => $data["zip"], 'Spieler.ort' => $data["location"], 'Spieler.email' => $data["email"], 'Spieler.geschlecht' => $data["gender"], 'Spieler.shirt' => $data["tshirt"]);
+		$before = $this -> Spieler -> find('count', array('conditions' => $query));
+		$this -> generate("Spielers") -> addmember($team_id, $data, $playernumber);
+		$after = $this -> Spieler -> find('count', array('conditions' => $query));
+		return $after - $before;
+	}
+
+	public function testAddMemberWrongDateOfBirth() {
+		$team_id = 1;
+		$playernumber = 1;
+		$firstname = "Johann";
+		$lastname = "Mustermann";
+		$dateofbirth = "05.10.2736";
+		$phone = "2342564";
+		$address = "Inffeldgasse";
+		$zip = 8010;
+		$location = "Graz";
+		$email = "johann.mustermann@email.at";
+		$gender = "m";
+		$tshirt = "s";
 		$data = array('firstname' => $firstname, 'secondname' => $lastname, 'dateofbirth' => $dateofbirth, 'phone' => $phone, 'address' => $address, 'zip' => $zip, 'location' => $location, 'email' => $email, 'gender' => $gender, 'tshirt' => $tshirt);
-		
-		$before = $this -> Spieler -> find('count', array('conditions' => array('Spieler.team_id' => $team_id, 'Spieler.spielernummer' => $spielernummer, 'Spieler.vorname' => $firstname, 'Spieler.nachname' => $lastname, 'Spieler.geburtsdatum' => $dateofbirth, 'Spieler.telefon' => $phone, 'Spieler.strasse' => $address, 'Spieler.plz' => $zip, 'Spieler.ort' => $location, 'Spieler.email' => $email, 'Spieler.geschlecht' => $gender, 'Spieler.shirt' => $tshirt)));
-		
-		$this -> generate("Spielers") -> addmember($team_id, $data, $spielernummer);
-		
 
-		//$this -> testAction('/Spielers/addmember.json', array('data' => $data, 'method' => 'post'));
-		$after = $this -> Spieler -> find('count', array('conditions' => array('Spieler.team_id' => $team_id, 'Spieler.spielernummer' => $spielernummer, 'Spieler.vorname' => $firstname, 'Spieler.nachname' => $lastname, 'Spieler.geburtsdatum' => $dateofbirth, 'Spieler.telefon' => $phone, 'Spieler.strasse' => $address, 'Spieler.plz' => $zip, 'Spieler.ort' => $location, 'Spieler.email' => $email, 'Spieler.geschlecht' => $gender, 'Spieler.shirt' => $tshirt)));
+		$this -> assertEqual($this -> insertMember($team_id, $data, $playernumber), 0);
+	}
 
-		// Test DB entry
-		$this -> assertEqual($after - $before, 1);
+	public function testAddMemberEmptyName() {
+		$team_id = 1;
+		$playernumber = 1;
+		$firstname = "";
+		$lastname = "Mustermann";
+		$dateofbirth = "05.10.2736";
+		$phone = "2342564";
+		$address = "Inffeldgasse";
+		$zip = 8010;
+		$location = "Graz";
+		$email = "johann.mustermann@email.at";
+		$gender = "m";
+		$tshirt = "s";
+		$data = array('firstname' => $firstname, 'secondname' => $lastname, 'dateofbirth' => $dateofbirth, 'phone' => $phone, 'address' => $address, 'zip' => $zip, 'location' => $location, 'email' => $email, 'gender' => $gender, 'tshirt' => $tshirt);
+
+		$this -> assertEqual($this -> insertMember($team_id, $data, $playernumber), 0);
 	}
 
 }
